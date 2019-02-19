e6ecb8fbcdac039b7e847de02753dda23c2146d8
JinXin <jxoday@163.com>
2019-02-19 18:01:44 +0800

    Initial commit

---
+++ app/src/main/java/com/jinxin/exercises/Layout/TableLayout.java
@@ -0,0 +1,18 @@
+package com.jinxin.exercises.Layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jinxin.exercises.R;
+
+/**
+ * 表格布局
+ */
+public class TableLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_table_layout);
+    }
+}
