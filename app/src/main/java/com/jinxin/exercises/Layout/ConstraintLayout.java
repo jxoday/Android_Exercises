4af588fe224c1d5d90782dcd029f58b8b3400f8a
JinXin <jxoday@163.com>
2019-03-18 23:54:58 +0800

    了解 Android 约束布局 ConstraintLayout 1、相对位置 2、尺寸约束

---
+++ app/src/main/java/com/jinxin/exercises/Layout/ConstraintLayout.java
@@ -0,0 +1,15 @@
+package com.jinxin.exercises.Layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jinxin.exercises.R;
+
+public class ConstraintLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_layout_a);
+    }
+}
